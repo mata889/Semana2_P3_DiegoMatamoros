d6f6c51103805cb73f09a30c1acb6c2d30ead68a
DiegoMatamoros <dam_900@unitec.edu>
2017-10-30 08:23:18 -0600

    Semana2

---
+++ miercoles.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+/*Introduccion a funciones*/
+
+/*devuelve la capacidad de almacenamiento del entero pasado como parametro*/
+
+int sizeofint(int);
+
+int main(){
+   int opcion;
+  cout<<"-----Menu----"<<endl
+  <<"1.Sumar"<<endl
+  <<"2.Salir"<<endl;
+  cout<<"Ingrese la opcion del menu"<<endl;
+  cin>>opcion;	  
+  
+}
+
+int suma(int a,int b){
+	
+
+}
+
